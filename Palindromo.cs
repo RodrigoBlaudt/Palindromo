302985d2123dc012a1dfc5877404ff5a82582ed1
RodrigoBlaudt <49947769+RodrigoBlaudt@users.noreply.github.com>
2020-12-05 12:33:55 -0300

    Initial commit

---
+++ Palindromo.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string i;
+            do
+            {
+                Console.WriteLine("Type a string with four characters: ");
+                i = Console.ReadLine();
+
+            } while(i.Length != 4);
+
+                if ((i[3] == i[0]) || (i[1] == i[2]))
+                {
+                    Console.WriteLine("The typed string it's a palindrome");
+                }
+                else
+                {
+                    Console.WriteLine("The typed string it's not a palindrome");
+                }
+        }
+    }
+}
